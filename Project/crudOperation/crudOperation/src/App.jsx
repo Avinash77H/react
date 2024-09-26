c1044c056c414515e65a2bf5fb47be818b5fe300
avinash <arakholiya5@gmail.com>
2024-09-26 10:20:04 +0530

    26/09/2024

---
+++ Project/crudOperation/crudOperation/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Operation from './Components/Operation'
+
+function App() {
+  
+
+  return (
+    <>
+    <Operation/>
+    </>
+  )
+}
+
+export default App
