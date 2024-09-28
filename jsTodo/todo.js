40e53f52e109ba039b9336f662340a3297a8dbbc
avinash <arakholiya5@gmail.com>
2024-09-28 10:56:31 +0530

    28/09/2024

--- jsTodo/todo.js
+++ jsTodo/todo.js
@@ -1,16 +1,38 @@
+let taskList = [];
 
-
-let input = document.querySelector("input-container input");
-
-
-let addTask = ()=>{
-    let newElement = document.createElement('p')
-    newElement.innerText = input.value
-    let newAddElement = document.querySelector('.task-container'); 
-
-    newTaskDiv = document.createElement('div').classList.add('newTaskContainer');
-    newAddElement.appendChild(newTaskContainer)
-    // console.log(newAddElement)
-    // newAddElement.appendChild(newElement)
+let input = document.querySelector(".input-container input");
+let  taskContainerElement = document.querySelector('.task-container');
+if(taskList.length === 0){
+    let newTaskChildContainerElement = document.createElement("div");
+    newTaskChildContainerElement.classList.add('newTaskChildContainer')
+    newTaskChildContainerElement.innerHTML = `<p class="emptyToDo">Enjoy your Day No More Task for Today</p>`
+    taskContainerElement.appendChild(newTaskChildContainerElement)
 }
 
+let addTask = () => {
+    if(taskList.length > 0){
+
+        let emptyToDoElement = document.querySelector('.emptyToDo')
+        emptyToDoElement.classList.add(".hidden")
+    }
+   
+
+    let newTaskChildContainerElement = document.createElement("div");
+  newTaskChildContainerElement.classList.add('newTaskChildContainer')
+  newTaskChildContainerElement.innerHTML= `<p class="task-title">${input.value}</p>
+  <div class="btn-container">
+      <button class="edit-btn">Edit</button>
+      <button class="delete-btn">Delete</button>
+  </div>`
+ 
+    taskList.push(newTaskChildContainerElement)
+    console.log(taskList)
+
+    taskList.map((task)=>{
+        taskContainerElement.appendChild(task)
+    })
+                
+    // taskContainerElement.appendChild(newTaskChildContainerElement)
+
+console.log(taskList)
+};
