60c550b30d6233086c5a47a50da6e8868ab7d37b
avinash <arakholiya5@gmail.com>
2024-10-02 11:47:58 +0530

    02.10.2024

--- Project/website/src/components/Accordion.jsx
+++ Project/website/src/components/Accordion.jsx
@@ -8,50 +8,203 @@ import Button from '@mui/material/Button';
 
 export default function AccordionUsage() {
   return (
+    <>
+
     <div>
+        <h3 className='text-xl pl-4 mb-2'>Shopping Information</h3>
+      {/* first accordion */}
       <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1-content"
           id="panel1-header"
+          className='hover:text-red-500'
         >
-          Accordion 1
+         <p className='text-sm'>Pellentesque habitant morbi tristique senectus et netus?</p>
         </AccordionSummary>
         <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        <p className='text-[11px] text-gray-600'>The perfect way to enjoy brewing tea on low hanging fruit to identify. Duis autem vel eum iriure dolor in hendrerit vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. For me, the most important part of improving at photography has been sharing it. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </AccordionDetails>
       </Accordion>
+
+      {/* second accordion */}
       <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2-content"
           id="panel2-header"
+          className='hover:text-red-500'
         >
-          Accordion 2
+         <p className='text-sm'>How much is shipping and how long will it take?</p>
         </AccordionSummary>
         <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          <p className='text-[11px] text-gray-600'>The perfect way to enjoy brewing tea on low hanging fruit to identify. Duis autem vel eum iriure dolor in hendrerit vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. For me, the most important part of improving at photography has been sharing it. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </AccordionDetails>
       </Accordion>
-      <Accordion defaultExpanded>
+
+      {/* third accordion */}
+      <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel3-content"
           id="panel3-header"
+          className='hover:text-red-500'
         >
-          Accordion Actions
+           <p className='text-sm'>How long will it take to get my package?</p>
         </AccordionSummary>
         <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+         <p className='text-[11px] text-gray-600'>The perfect way to enjoy brewing tea on low hanging fruit to identify. Duis autem vel eum iriure dolor in hendrerit vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. For me, the most important part of improving at photography has been sharing it. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </AccordionDetails>
-        <AccordionActions>
-          <Button>Cancel</Button>
-          <Button>Agree</Button>
-        </AccordionActions>
+        
+      </Accordion>
+
+       {/* fourth accordion */}
+       <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+          className='hover:text-red-500'
+        >
+           <p className='text-sm'>Branding is simply a more efficient way to sell things?</p>
+        </AccordionSummary>
+        <AccordionDetails>
+         <p className='text-[11px] text-gray-600'>The perfect way to enjoy brewing tea on low hanging fruit to identify. Duis autem vel eum iriure dolor in hendrerit vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. For me, the most important part of improving at photography has been sharing it. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </AccordionDetails>
+        
       </Accordion>
     </div>
+
+    <div className='mt-12'>
+        <h3 className='text-xl pl-4'>Payment Information</h3>
+      {/* first accordion */}
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+          className='hover:text-red-500'
+        >
+         <p className='text-sm'>Pellentesque habitant morbi tristique senectus et netus?</p>
+        </AccordionSummary>
+        <AccordionDetails>
+        <p className='text-[11px] text-gray-600'>The perfect way to enjoy brewing tea on low hanging fruit to identify. Duis autem vel eum iriure dolor in hendrerit vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. For me, the most important part of improving at photography has been sharing it. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </AccordionDetails>
+      </Accordion>
+
+      {/* second accordion */}
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+          className='hover:text-red-500'
+        >
+         <p className='text-sm'>How much is shipping and how long will it take?</p>
+        </AccordionSummary>
+        <AccordionDetails>
+          <p className='text-[11px] text-gray-600'>The perfect way to enjoy brewing tea on low hanging fruit to identify. Duis autem vel eum iriure dolor in hendrerit vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. For me, the most important part of improving at photography has been sharing it. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </AccordionDetails>
+      </Accordion>
+
+      {/* third accordion */}
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+          className='hover:text-red-500'
+        >
+           <p className='text-sm'>How long will it take to get my package?</p>
+        </AccordionSummary>
+        <AccordionDetails>
+         <p className='text-[11px] text-gray-600'>The perfect way to enjoy brewing tea on low hanging fruit to identify. Duis autem vel eum iriure dolor in hendrerit vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. For me, the most important part of improving at photography has been sharing it. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </AccordionDetails>
+        
+      </Accordion>
+
+       {/* fourth accordion */}
+       <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+          className='hover:text-red-500'
+        >
+           <p className='text-sm'>Branding is simply a more efficient way to sell things?</p>
+        </AccordionSummary>
+        <AccordionDetails>
+         <p className='text-[11px] text-gray-600'>The perfect way to enjoy brewing tea on low hanging fruit to identify. Duis autem vel eum iriure dolor in hendrerit vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. For me, the most important part of improving at photography has been sharing it. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </AccordionDetails>
+        
+      </Accordion>
+    </div> 
+
+    <div className='mt-12'>
+        <h3 className='text-xl pl-4'>Order Returns</h3>
+      {/* first accordion */}
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+          className='hover:text-red-500'
+        >
+         <p className='text-sm'>Pellentesque habitant morbi tristique senectus et netus?</p>
+        </AccordionSummary>
+        <AccordionDetails>
+        <p className='text-[11px] text-gray-600'>The perfect way to enjoy brewing tea on low hanging fruit to identify. Duis autem vel eum iriure dolor in hendrerit vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. For me, the most important part of improving at photography has been sharing it. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </AccordionDetails>
+      </Accordion>
+
+      {/* second accordion */}
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+          className='hover:text-red-500'
+        >
+         <p className='text-sm'>How much is shipping and how long will it take?</p>
+        </AccordionSummary>
+        <AccordionDetails>
+          <p className='text-[11px] text-gray-600'>The perfect way to enjoy brewing tea on low hanging fruit to identify. Duis autem vel eum iriure dolor in hendrerit vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. For me, the most important part of improving at photography has been sharing it. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </AccordionDetails>
+      </Accordion>
+
+      {/* third accordion */}
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+          className='hover:text-red-500'
+        >
+           <p className='text-sm'>How long will it take to get my package?</p>
+        </AccordionSummary>
+        <AccordionDetails>
+         <p className='text-[11px] text-gray-600'>The perfect way to enjoy brewing tea on low hanging fruit to identify. Duis autem vel eum iriure dolor in hendrerit vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. For me, the most important part of improving at photography has been sharing it. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </AccordionDetails>
+        
+      </Accordion>
+
+       {/* fourth accordion */}
+       <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+          className='hover:text-red-500'
+        >
+           <p className='text-sm'>Branding is simply a more efficient way to sell things?</p>
+        </AccordionSummary>
+        <AccordionDetails>
+         <p className='text-[11px] text-gray-600'>The perfect way to enjoy brewing tea on low hanging fruit to identify. Duis autem vel eum iriure dolor in hendrerit vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. For me, the most important part of improving at photography has been sharing it. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </AccordionDetails>
+        
+      </Accordion>
+    </div> 
+
+    </>
   );
 }
--- Project/website/src/pages/FAQ.jsx
+++ Project/website/src/pages/FAQ.jsx
@@ -12,9 +12,24 @@ function FAQ() {
           <h1 className="font-normal text-4xl">FAQ 01</h1>
       </div>
 
-    {/* accordion */}
-    
+    {/* accordion section */}
+    <div className='border-2 border-purple-500 mt-20 mx-8 flex'>
+
+      {/* accordion container */}
+      <div className='w-[60%]'>
     <Accordion/>
+      </div>
+
+      {/* right box */}
+      <div className='w-[40%] border-2 border-orange-500'>
+        <div>
+        <h3 className='text-xl pl-4 mb-2'>Have a question</h3>
+        <p className='text-[11px] text-gray-600'>If you have an issue or question that requires immediate assistance, you can click the button below to chat live with a Customer Service representative.</p>
+        <p className='text-[11px] text-gray-600'>Please allow 06 - 12 business days from the time your package arrives back to us for a refund to be issued.</p>
+        </div>
+      </div>
+    </div>
+    
 
 
     </div>
