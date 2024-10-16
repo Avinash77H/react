bc4d6a14fdad61e23ebba345d6fdf533fc0540db
Avinash77H <arakholiya5@gmail.com>
2024-10-16 17:51:40 +0530

    16.10.2024

---
+++ miniProject/counterRedux/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Counter from "./Components/Counter";
+
+function App() {
+  return <div>
+    <Counter/>
+  </div>;
+}
+
+export default App;
