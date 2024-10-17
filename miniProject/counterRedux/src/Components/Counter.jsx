8ac2a379487b028e76ed195de4be50ebae585ce0
Avinash77H <arakholiya5@gmail.com>
2024-10-17 14:24:31 +0530

    17.10.2024

--- miniProject/counterRedux/src/Components/Counter.jsx
+++ miniProject/counterRedux/src/Components/Counter.jsx
@@ -8,7 +8,7 @@ function Counter() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <div className='flex justify-center items-center gap-4'>
-      <button onClick={()=>dispatch(decrement())} className='bg-red-500 text-white px-2 py-1 rounded-md'>Decrement</button> 
+      <button onClick={()=>dispatch(decrement())} className='bg-red-500 text-white  px-2 py-1 rounded-md'>Decrement</button> 
       
       <div className='text-2xl '>{count}</div>
       <button onClick={()=>dispatch(increment())} className='bg-green-500 text-white px-2 py-1 rounded-md'>Increment</button>
