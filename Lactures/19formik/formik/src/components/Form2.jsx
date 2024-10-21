0f564bd26c5aa87d438ae6b146b97093806af71b
Avinash77H <arakholiya5@gmail.com>
2024-10-21 13:38:55 +0530

    21.10.2024

--- Lactures/19formik/formik/src/components/Form2.jsx
+++ Lactures/19formik/formik/src/components/Form2.jsx
@@ -22,24 +22,7 @@ function Form2() {
 
   })
 
-  // const validate = (values)=>{
-  //   const errors = {}
-  //   if(!values.fullName){
-  //     errors.fullName = "Require"
-  //   }
-  //   if(!values.email){
-  //     errors.email = "Require"
-  //   }else if (
-  //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-  //   ) {
-  //     errors.email = 'Invalid email address';
-  //   }
-  //   if(!values.password){
-  //     errors.password = "Require"
-  //   }
-  //   return errors
-  // }
-
+  
   let formik = useFormik({
     initialValues,
     onSubmit,
