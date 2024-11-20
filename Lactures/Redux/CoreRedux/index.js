9f5048bc777fc454e23cb6ac46dc966ad2359b5e
avinash <arakholiya5@gmail.com>
2024-11-20 11:10:47 +0530

    learn core redux

---
+++ Lactures/Redux/CoreRedux/index.js
@@ -0,0 +1,31 @@
+const redux = require("redux")
+
+const createStore = redux.createStore;
+
+const initialState = {
+    chocolate:10,
+    cake: 17,
+    drink:7
+}
+
+const reducer = (state=initialState,action)=>{
+    switch (action.type) {
+        case 'BUY_CHOCOLATE': return{
+            ...state,chocolate:state.chocolate - 1
+        }
+        case 'BUY_CAKE': return{
+            ...state,cake:state.cake - 1
+        }
+        case 'BUY_DRINK': return{
+            ...state,drink:state.drink - 1
+        }
+        default: return state;
+    }
+}
+
+const store = createStore(reducer)
+
+console.log(store.getState())
+
+store.dispatch({type:'BUY_CAKE'})
+console.log(store.getState())
