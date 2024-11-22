fe88c1853061c8b9a4983750ff2b62aa0bd8314d
avinash <arakholiya5@gmail.com>
2024-11-22 11:49:37 +0530

    update logger concept

--- Lactures/Redux/CoreRedux/index.js
+++ Lactures/Redux/CoreRedux/index.js
@@ -1,4 +1,9 @@
 const redux = require("redux")
+const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+
+const reduxLogger = require("redux-logger")
+const logger = reduxLogger.createLogger()
 
 const createStore = redux.createStore;
 
@@ -8,26 +13,51 @@ const initialState =  {
     drink:7
 }
 
-const reducer = (state=initialState,action)=>{
-    switch (action.type) {
-        case 'BUY_CHOCOLATE': return{
+// const reducer = (state=initialState,action)=>{
+//     switch (action.type) {
+//         case 'BUY_CHOCOLATE': return{
+//             ...state,chocolate:state.chocolate - 1
+//         }
+//         case 'BUY_CAKE': return{
+//             ...state,cake:state.cake - 1
+//         }
+//         case 'BUY_DRINK': return{
+//             ...state,drink:state.drink - 1
+//         }
+//         default: return state;
+//     }
+// }
+
+
+const chocoReducer = (state=initialState,action)=>{
+    if(action.type === 'BUY_CHOCOLATE'){
+        return {
             ...state,chocolate:state.chocolate - 1
         }
-        case 'BUY_CAKE': return{
-            ...state,cake:state.cake - 1
-        }
-        case 'BUY_DRINK': return{
-            ...state,drink:state.drink - 1
-        }
-        default: return state;
     }
+    return state;
 }
 
+const cakeReducer = (state=initialState,action)=>{
+    if(action.type === 'BUY_CAKE'){
+        return {
+            ...state,cake:state.cake - 1
+        }
+    }
+    return state;
+}
 
-const store = createStore(reducer)
+const rootReducer = combineReducers({
+  chocoReducer,
+  cakeReducer
+})
+
+
+const store = createStore(rootReducer,applyMiddleware(logger))
 console.log(store.getState())
 
 store.dispatch({type:'BUY_CAKE'})
-store.dispatch({type:'BUY_CAKE'})
-store.dispatch({type:'BUY_CAKE'})
+// store.dispatch({type:'BUY_CAKE'})
+// store.dispatch({type:'BUY_CAKE'})
 console.log(store.getState())
+
