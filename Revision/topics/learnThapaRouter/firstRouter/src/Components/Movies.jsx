f41ab6d8ce7e40bcacd2bbcd1abdb0182df48ede
vijay dankhara <vijaydankhara111@gmail.com>
2025-01-01 13:58:26 +0530

    ...

--- Revision/topics/learnThapaRouter/firstRouter/src/Components/Movies.jsx
+++ Revision/topics/learnThapaRouter/firstRouter/src/Components/Movies.jsx
@@ -9,7 +9,7 @@ function Movies() {
     <div>
     <h1 className='header'>movies</h1>
     <ul className='grid grid-cols-3 gap-4 mt-4 px-4'>
-      {data.Search.map((item,index)=><Card key={item.imdbID} item={item}/>)}
+      {data.Search.map((item)=><Card key={item.imdbID} item={item}/>)}
     </ul>
     <Outlet/>
     </div>
