2bc71dcd450faaed326f536786fbbb433a694658
vijay dankhara <vijaydankhara111@gmail.com>
2025-01-03 13:58:56 +0530

    ...

--- Revision/topics/learnThapaRouter/firstRouter/src/Components/GetMovieData.jsx
+++ Revision/topics/learnThapaRouter/firstRouter/src/Components/GetMovieData.jsx
@@ -1,7 +1,7 @@
 
 
 async function GetMovieData() {
- const response = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=venom&page=1");
+ const response = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=venom&page=2");
  const data = response.json()
  return data;
 }
--- Revision/topics/learnThapaRouter/firstRouter/src/Components/MovieDetails.jsx
+++ Revision/topics/learnThapaRouter/firstRouter/src/Components/MovieDetails.jsx
@@ -4,8 +4,7 @@ import MovieDetailsCard from './MovieDetailsCard';
 
 function MovieDetails() {
   const data = useLoaderData();
-  // const {id} = useParams();
-  // const newData = data.Search.filter((item)=>item.imdbID.includes(id));
+
   return (
     <div>
       <h1 className='header'>Movie Details</h1>
@@ -16,8 +15,3 @@ function MovieDetails() {
 
 export default MovieDetails
 
-// export async function GEtMovieDataDetails(){
-//   const response = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=venom&page=1");
-//   const data = response.json()
-//   return data;
-// }
